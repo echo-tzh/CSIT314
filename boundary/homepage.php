1cb399021c927985f9e9627cc3ee3c486d997fc7
echo-tzh <76261514+echo-tzh@users.noreply.github.com>
2025-04-16 23:58:07 +0800

    Secured homepage.php, cannot be entered by just typing in URL, logout.php completed, session is ended when logged out

--- boundary/homepage.php
+++ boundary/homepage.php
@@ -1,5 +1,15 @@
+
+<?php
+session_start();
+if (!isset($_SESSION["username"])) {
+    header("Location: loginPage.php"); // Redirect if not logged in
+    exit();
+}
+?>
+
+
 <html>
     <h1>Welcome to home page</h1>
 
-    
+    <a href="logoutPage.php" class="btn logout-btn">Logout</a>
 </html>
