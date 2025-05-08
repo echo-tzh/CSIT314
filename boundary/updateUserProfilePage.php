31ce56d4a8d6c3e42b5735c6181d0a200eec37ae
darrenngggggg <darrenngwenxiang@gmail.com>
2025-05-09 02:49:03 +0800

    Update update user profile page field description

--- boundary/updateUserProfilePage.php
+++ boundary/updateUserProfilePage.php
@@ -67,7 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         ?>
         <form action="" method="post" class="space-y-4">
             <div>
-                <label for="userProfileName" class="block font-medium text-gray-700">Name:</label>
+                <label for="userProfileName" class="block font-medium text-gray-700">User Profile Name:</label>
                 <input type="text" id="userProfileName" name="userProfileName" value="<?php echo htmlspecialchars($profile['userProfileName']); ?>" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
             </div>
             <div>
